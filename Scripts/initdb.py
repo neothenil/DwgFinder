1d336c1a5ac067e774628e6a5e260599173d089a
刘裕 <liuyu@zwcad.com>
2023-03-16 16:57:00 +0800

    initdb.py适配修改

--- Scripts/initdb.py
+++ Scripts/initdb.py
@@ -9,32 +9,13 @@ with open(config_file, encoding="utf8") as f:
 
 dbPath = config["db_path"]
 tableName = config["db_table"]
-tableColumns = []
-
-
-def readTableColumns():
-    with open("CountEntity/classInfo.cpp") as f:
-        lines = f.readlines()
-    startLine = 0
-    for lineNum, line in enumerate(lines):
-        if "gClassNames" in line:
-            startLine = lineNum + 1
-            break
-    i = startLine
-    while "};" not in lines[i]:
-        if lines[i].strip(' ",\r\n').startswith("//"):
-            i += 1
-            continue
-        className = lines[i].strip(' ",\r\n')
-        tableColumns.append(className)
-        i += 1
+tableColumns = config["db_columns"]
 
 
 def createTable():
     db = sqlite3.connect(dbPath)
     cur = db.cursor()
     cur.execute(f"DROP TABLE IF EXISTS {tableName}")
-    readTableColumns()
     strColumns = "id INTEGER PRIMARY KEY AUTOINCREMENT," + "path TEXT UNIQUE"
     for clsName in tableColumns:
         strColumns += f",{clsName} INTEGER NOT NULL"
